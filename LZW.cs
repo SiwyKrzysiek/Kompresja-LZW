9a60eaffaa8685042d53e851d60fb302d23f94ed
SiwyKrzysiek <k.fajny@gmail.com>
2018-11-20 17:38:39 +0100

    Prawie skończone kompresowanie Głównie przepisanie kodu C++ z zajęć na C#

---
+++ LZW.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LZW_Compersion
+{
+    using DataBlock = UInt32; //Ustalenie jak będą zapisywane kody
+
+    public static class LZW
+    {
+        public static DataBlock[] Compress(string data)
+        {
+            if (DataBlock.MaxValue <= char.MaxValue)
+                throw new Exception("To small DataBlock set in code");
+            if (data.Length == 0) return new DataBlock[0]; //Edge case dla pustych danych
+
+            List<DataBlock> result = new List<DataBlock>(); //Wynik kodowania
+            Dictionary<string, DataBlock> dictionary = CreateDictionary(); //Utworzenie początkowego słownika
+
+            string currentSequence = data[0].ToString(); //Wczytanie pierwszego znaku do kolejki aktualnie kodowanych znaków
+            for (int i = 1; i < data.Length; i++) //Od 2 elementu, bo piewszy już został wczytany
+            {
+                string sequenceWithNewSymbol = currentSequence + data[i]; //Ciąg z aktualnie przetwarzanym znakiem
+
+                if (dictionary.ContainsKey(sequenceWithNewSymbol)) //Sprawdzenie czy przedłużony ciąg jest w słowniku
+                {
+                    currentSequence = sequenceWithNewSymbol;
+                }
+                else //Nie ma kodu dla rozszeżonego ciągu
+                {
+                    result.Add(dictionary[currentSequence]); //Wypisanie kodu dla ciągu, który do tej pory był w słowniki
+                    dictionary.Add(sequenceWithNewSymbol, (DataBlock)dictionary.Count);
+                }
+            }
+        }
+
+        private static Dictionary<string, DataBlock> CreateDictionary() //Wypełani słownik pojedyńczymi znakami
+        {
+            Dictionary<string, DataBlock> dictionary = new Dictionary<string, DataBlock>();
+
+            for (char i = char.MinValue; i <= char.MaxValue; i++) //Wpisanie wszystkich możliwych pojednyńczych znaków do słownika
+                dictionary.Add(i.ToString(), (DataBlock)dictionary.Count); //Generowanie kodów polega na przypisaniu aktualnej wielkości słownika
+                                                                           //Ponieważ słownik nigdy nie maleje kody są unikalne
+            return dictionary;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿//Program kopresujący algorytmem LZW
+//Na lagoratoria z Algorytmów i Systemów Operacyjnych
+//Autor: Krzysztof Dąbrowski
+//
+//Przejście na C#
+
+using System;
+using System.IO;
+
+namespace LZW_Compersion
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //StreamReader data = new StreamReader("text.txt");
+
+            //Console.WriteLine(data.ReadLine());
+
+            //data.Close();
+
+            string data = File.ReadAllText(@"test.txt");
+
+
+            //string myFile = File.ReadAllText("test.txt");
+            //Console.WriteLine(myFile);
+        }
+    }
+}

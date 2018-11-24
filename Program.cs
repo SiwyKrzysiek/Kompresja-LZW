14ac2ca3adf8ec58c5708e2d7d99a659ff255dd7
Krzysztof Dąbrowski <k.fajny@gmail.com>
2018-11-25 00:02:17 +0100

    Przeczyszczenie maina

--- Program.cs
+++ Program.cs
@@ -5,7 +5,6 @@
 //Przejście na C#
 
 using System;
-using System.IO;
 
 namespace LZW_Compersion
 {
@@ -15,39 +14,7 @@ namespace LZW_Compersion
     {
         public static void Main(string[] args)
         {
-            //FileInfo toTest = new FileInfo("text.txt");
-            //Console.WriteLine($"File: {toTest.Name} Size: {toTest.Length}");
-
-            string imputFile = "iliada.txt";
-            string outputFile = "compressed.lzw";
-
-            string data = File.ReadAllText(imputFile);
-
-
-            DataBlock[] compressed = LZW.Compress(data);
-
-            using (BinaryWriter writer = new BinaryWriter(File.Open(outputFile, FileMode.Create)))
-            {
-                foreach (DataBlock block in compressed)
-                {
-                    writer.Write(block);
-                }
-            }
-
-            long inputFileSize = new FileInfo(imputFile).Length;
-            long outputFileSize = new FileInfo(outputFile).Length;
-            double compressionDegree = (double)inputFileSize / outputFileSize * 100;
-
-            Console.WriteLine($"Dane z pliku {imputFile} zostały skompresowane i zapisane w pliku {outputFile}");
-            Console.WriteLine($"Wielkość pliku wejściowego: {inputFileSize / 1000} KB");
-            Console.WriteLine($"Wielkość pliku wyjściowego: {outputFileSize / 1000} KB");
-            Console.WriteLine($"Współczynnik kompresjii wynosi: {compressionDegree:0.00}%");
-            
-
-            //Console.WriteLine(LZW.Decompress(compressed));
-
-            //string myFile = File.ReadAllText("test.txt");
-            //Console.WriteLine(myFile);
+            ConsoleInterface.HandleCommandlineArguments(args);
         }
     }
 }
